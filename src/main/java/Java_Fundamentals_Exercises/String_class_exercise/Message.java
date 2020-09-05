5551c2856570ba2390782d545e125bc5d59620d0
Fotis Koumidis <fkoumidis@gmail.com>
2020-09-05 18:42:08 +0300

    corrections

--- src/main/java/Java_Fundamentals_Exercises/String_class_exercise/Message.java
+++ src/main/java/Java_Fundamentals_Exercises/String_class_exercise/Message.java
@@ -3,7 +3,7 @@ package Java_Fundamentals_Exercises.String_class_exercise;
 public class Message {
 
     public boolean invalid(){
-        System.out.println("Invalid password\n\n"+"Follow the below criteria for  password:\n+" +
+        System.out.println("Invalid password\n\n"+"Follow the below criteria for  password:\n" +
                 "1. Password contains at least one uppercase character\n" +
                 "2. Password contains at least one lowercase character\n" +
                 "3. Contains at least one number\n" +
